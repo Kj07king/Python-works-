24234e35f0dc0bc1e98a272a95620f49be60eaf7
Kj07king <kabirkimtani108@gmail.com>
2025-07-02 22:45:35 +0400

    Add files via upload

---
+++ Pythoncalculator.py
@@ -0,0 +1,14 @@
+A=int(input("Enter First value:"))
+B=int(input("Enter Second Value:"))
+C=input("type a Arithmetic operation:")
+
+if C=="/":
+     print(A/B)
+elif C=="+":
+     print(A+B)
+elif C=="-":
+     print(A-B)
+elif C=="*":
+    print(A*B)
+else:
+     print("No Arithmetic operation given")
